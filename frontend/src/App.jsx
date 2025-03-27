9212525c6769acac9aaa09a519f02030ab77f486
edgarespinoza7 <edgarespinoza7@gmail.com>
2025-03-27 10:51:31 +0100

    updated Login and Register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,17 @@
-import React from 'react'
-import { Outlet } from 'react-router'
-import './App.css'
-import { Navbar } from './components/Navbar'
+import React from "react";
+import { Outlet } from "react-router";
+import "./App.css";
+import { Navbar } from "./components/Navbar";
 
 function App() {
-
-
   return (
     <>
       <Navbar />
-      <main className='min-h-screen max-w-screen-2xl mx-auto'>
+      <main className="min-h-screen max-w-screen-2xl mx-auto">
         <Outlet />
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,10 @@
-import React, { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 import { RouterProvider } from "react-router";
-import router from './routers/router.jsx'
+import router from "./routers/router.jsx";
 
-
-
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>
