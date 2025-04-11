300478a1284c6b86e5884273c218860bbf49a4d6
edgarespinoza7 <edgarespinoza7@gmail.com>
2025-04-11 15:40:25 +0200

    updated UpdateBook

--- frontend/src/pages/dashboard/editBook/UpdateBook.jsx
+++ frontend/src/pages/dashboard/editBook/UpdateBook.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import InputField from '../addBook/InputField'
 import SelectField from '../addBook/SelectField'
 import { useForm } from 'react-hook-form';
-import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router';
 import { useFetchBookByIdQuery, useUpdateBookMutation } from '../../../redux/features/books/booksApi';
 import Loading from '../../../components/Loading';
 import Swal from 'sweetalert2';
